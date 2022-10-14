cc7efb6db575220c3fabaff77db05fae1e7098e0
mattialicausi <licausivitomattia@gmail.com>
2022-10-14 14:27:49 +0200

    creazione repo

---
+++ libreria.js
@@ -0,0 +1,35 @@
+// LIBRERIA FUNZIONI
+
+function randomNumber(min, max) {
+    //console.log(arguments);//
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+function isEven(num){
+    if(num % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function notificationError(msgError){
+    const alerta = document.createElement('div');
+    alerta.className='alert alert-danger';
+    alerta.innerHTML = msgError;
+    return alerta;
+}
+
+function removeFirstNotification(){
+    const alertToRemove = document.querySelector('.alert');
+    console.log(alertToRemove);
+   if(alertToRemove) alertToRemove.remove();
+}
+// const parola = 'pippo';
+// const parolaLen = parola.length;
+// let parolaGirata = parola.split('').reverse().join('');
+// for(let i = parolaLen - 1; i >= 0; i-- ){
+//     parolaGirata += parola.charAt(i);
+// }
+// console.log(parolaGirata);
+
+
